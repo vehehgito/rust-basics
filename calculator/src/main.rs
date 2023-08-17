c7c99a50e7ff0fcad4f8301b9e2ed9da7ae04794
vehehgito <aryann2708@gmail.com>
2023-08-18 00:48:21 +0530

    Started work on calculator

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -5,5 +5,14 @@ fn main() {
 
     loop{
         println!("Enter an operator (+, -, *, /) or q to quit");
+        let mut operator = String::new();
+        io::stdin().read_line(&mut operator).expect("Failed to read line");
+        let operator = operator.trim();
+
+        if operator == "q"{
+            break;
+        }
+
+        println!("Enter the first number");
     }
 }
