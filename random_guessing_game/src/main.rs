5ca6df1e3a866b7dfe9e9415f68f23572fb828e9
vehehgito <aryann2708@gmail.com>
2023-09-18 10:35:15 +0530

    Started work on guessing game

--- random_guessing_game/src/main.rs
+++ random_guessing_game/src/main.rs
@@ -2,6 +2,22 @@ use std::io;
 use rand::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let answer = rng.gen_range(0..101);
+
+    let mut guess = String::new();
+    println!("Guess the number between 1-100");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    if guess == answer{
+        println!("You win! The number was {}",answer);
+    }
+
+    else {
+        println!("You lose!!");
+    }
+
+    
     
 }
